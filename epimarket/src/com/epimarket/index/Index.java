7a289eada7920a41efc98b6b69d425bd43b68a55
minette.alexandre <minette.alexandre@gmail.com>
2011-12-16 09:29:30 +0000

    le com de ouuuf.

--- epimarket/src/com/epimarket/index/Index.java
+++ epimarket/src/com/epimarket/index/Index.java
@@ -1,11 +1,16 @@
 package com.epimarket.index;
 
 import java.io.IOException;
+
+import javax.persistence.PersistenceContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.epimarket.database.EMF;
+import com.epimarket.test.Auteur;
 
 /**
  * Servlet implementation class Index
@@ -28,11 +33,14 @@ public class Index extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().write("MIAM");
+		Auteur a = new Auteur();
+		a.setName("miam");
+		EMF.save(a);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 */ 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
