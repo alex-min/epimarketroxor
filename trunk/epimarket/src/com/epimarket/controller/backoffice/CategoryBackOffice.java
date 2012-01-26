64098ab8e636d6047b303c367bd9748de186a2a8
minette.alexandre@gmail.com <minette.alexandre@gmail.com@6b034a65-9cfd-4bcd-27b7-06d05680507d>
2012-01-26 08:49:20 +0000

    ajout des categories

--- trunk/epimarket/src/com/epimarket/controller/backoffice/CategoryBackOffice.java
+++ trunk/epimarket/src/com/epimarket/controller/backoffice/CategoryBackOffice.java
@@ -2,8 +2,11 @@ package com.epimarket.controller.backoffice;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Controller;
@@ -15,81 +18,91 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.epimarket.database.EMF;
 import com.epimarket.entity.Book;
-import com.epimarket.entity.User;
-import com.epimarket.webdata.WD;
-import com.epimarket.webdata.WebUser;
-
+import com.epimarket.entity.Category;
 
 @Controller
-public class UserBackOffice {
-	// REDIRECT admin -> admin/
-	/*@RequestMapping(
-			value = "admin",
-			method = RequestMethod.GET)
-	public String redirect(HttpServletRequest rqst, HttpServletResponse resp, Model model) {
-		return "redirect:app/admin/";
-	}*/
+public class CategoryBackOffice {
+
+	@RequestMapping(value = "ajax/change/category/{type}/{id}",
+			method=RequestMethod.POST)
+	public String getList
+	(HttpServletRequest rqst, HttpServletResponse resp, Model model,
+			@PathVariable("type") String type,
+			@PathVariable("id") int id,
+			@RequestParam("data") String data
+			) {
+		Criteria crit = EMF.getSession().createCriteria(Category.class);
+		crit.add(Restrictions.eq("id", id));
+		Category cat = (Category) crit.uniqueResult();
+		try {
+			System.out.println("type:" + type);
+
+			PropertyUtils.setSimpleProperty(cat, type,
+					data
+					);
+			EMF.update(cat);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "raw";
+	}
 
 	@RequestMapping(
-			value = "admin/user/",
+			value = "admin/category/",
 			method = RequestMethod.GET)
 	public ModelAndView all()
 	{
-		WD.getData().getUser().setRight(WebUser.ADMIN);
-		if (WD.getData().getUser().getRight() != WebUser.ADMIN)
-		{
-			EMF.getHttpSession().setAttribute("lasterror", "Access Denied");
-			return new ModelAndView("processerror");
-		}
-
-		Criteria crit = EMF.getSession().createCriteria(User.class);
+		Criteria crit = EMF.getSession().createCriteria(Category.class);
 		@SuppressWarnings("unchecked")
 		List<Book> list = crit.list();
-		EMF.getHttpSession().setAttribute("users", list);
-		return new ModelAndView("backoffice_user", "useradd", new User());
+		EMF.getHttpSession().setAttribute("categories", list);
+		return new ModelAndView("backoffice_category", "categoryadd", new Category());
 	}
 
 
 	@RequestMapping(
-			value = "admin/user/delete/{id}/",
+			value = "admin/category/delete/{id}/",
 			method = RequestMethod.GET)
 	public String addToCart(Model model, @PathVariable("id") int id)
 	{
 		// TODO : uncomment
-		/* if ((WD.getData().getUser().getRight() != WebUser.ADMIN))
+		/* if ((WD.getData().getCategory().getRight() != WebCategory.ADMIN))
 		{
 			EMF.getHttpSession().setAttribute("lasterror", "Access Denied");
 			return "processerror";
 		} */
 
-		Criteria c = EMF.getSession().createCriteria(User.class);
+		Criteria c = EMF.getSession().createCriteria(Category.class);
 		c.add(Restrictions.eq("id", id));
-		User b = (User) c.uniqueResult();
+		Category b = (Category) c.uniqueResult();
 		if (b != null)
 		{
 			try {
 				EMF.getSession().delete(b);
 			} catch (Exception e) {}
 		} else { System.out.println("Uset is null"); }
-		return "redirect:/app/admin/user/";
+		return "redirect:/app/admin/category/";
 	}
 
 	@RequestMapping(
-			value = "useradd",
+			value = "categoryadd",
 			method = RequestMethod.GET)
 	public ModelAndView showContacts() {
-		return new ModelAndView("admin", "bookadd", new Book());
+		return new ModelAndView("admin", "bookadd", new Category());
 	}
 
-	@RequestMapping(value="admin/user/useraddaction",
+	@RequestMapping(value="admin/category/categoryaddaction",
 			method=RequestMethod.POST)
-	public String addContact(@ModelAttribute("user")
-	@Valid User user, BindingResult result, ModelMap m) {
-		System.out.println("user : " + user);
+	public String addContact(@ModelAttribute("category")
+	@Valid Category category, BindingResult result, ModelMap m) {
+		System.out.println("category : " + category);
 		EMF.getHttpSession().setAttribute("lasterror", "");
 		if (result.hasErrors()) {
 			String errorOutput = "Registration failed : </br>";
@@ -98,14 +111,14 @@ public class UserBackOffice {
 				errorOutput += a.getObjectName() + " : " + a.getDefaultMessage() + "</br>";
 			}
 			EMF.getHttpSession().setAttribute("lasterror", errorOutput);
-			return "redirect:/app/admin/user/";
+			return "redirect:/app/admin/category/";
 		}
 		try {
 			EMF.begin();
-			EMF.save(user);
+			EMF.save(category);
 		} catch (Exception e) {
 			m.addAttribute("error", e.getMessage());
 		}
-		return "redirect:/app/admin/user/";
+		return "redirect:/app/admin/category/";
 	}
 }
