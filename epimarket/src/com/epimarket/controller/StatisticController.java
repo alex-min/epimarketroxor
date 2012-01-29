dface7fa20075c3e5bf8ae0a470c3adb16ff95b4
fredericcolin.fc <fredericcolin.fc@6b034a65-9cfd-4bcd-27b7-06d05680507d>
2012-01-29 09:46:17 +0000

    stats semestre et semaine

--- epimarket/src/com/epimarket/controller/StatisticController.java
+++ epimarket/src/com/epimarket/controller/StatisticController.java
@@ -14,9 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.entity.StandardEntityCollection;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.servlet.ServletUtilities;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 import org.springframework.stereotype.Controller;
@@ -37,17 +40,19 @@ public class StatisticController
 	public String monthly(HttpServletRequest rqst, HttpServletResponse resp, Model model)
 	{
 		Criteria				crit		= null;
-		DefaultPieDataset		pieDataSet	= new DefaultPieDataset();
+		//DefaultPieDataset		pieDataSet	= new DefaultPieDataset();
 		GregorianCalendar		cal			= new GregorianCalendar();
 		SimpleDateFormat		fo			= new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
 		cal.add(Calendar.DAY_OF_WEEK, 1);
 		Date	end	= cal.getTime();
-		cal.add(Calendar.MONTH, -7);
+		cal.add(Calendar.MONTH, -6);
 		Date	begin = cal.getTime();
 		//System.out.println("begin:" + begin);
 		//System.out.println("end:" + end);
 		
+		String		pieValue	= new String();
+		String		pieName		= new String();
 		for (Integer i = 1; i < 5; ++i) {
 			crit = EMF.getSession().createCriteria(Purchase.class.getCanonicalName());
 			crit.add(Restrictions.eq("nbArticles", i));
@@ -56,7 +61,9 @@ public class StatisticController
 			} catch (ParseException e) { e.printStackTrace(); }
 			List<?>		l = crit.list();
 			//System.out.println("ICCICIIIIIIIIIIIIIIIIIIi =========>" + l.size());
-			pieDataSet.setValue(i.toString(), new Integer(l.size()));
+			//pieDataSet.setValue(i.toString(), new Integer(l.size()));
+			pieValue += ((Integer)l.size()).toString() + ",";
+			pieName += i.toString() + "|";
 			crit.list().clear();
 		}
 		crit = EMF.getSession().createCriteria(Purchase.class.getCanonicalName());
@@ -64,26 +71,19 @@ public class StatisticController
 		try {
 			crit.add(Restrictions.between("date", (Date)(fo.parse(fo.format(begin))), (Date)(fo.parse(fo.format(end)))));
 		} catch (ParseException e) { e.printStackTrace(); }
-		pieDataSet.setValue("5 et +", new Integer(crit.list().size()));
-		JFreeChart chart = ChartFactory.createPieChart
-				("Number book on a command during the last 6 months", // Title
-				pieDataSet, // Dataset
-				true, // Show legend
-				true, // Use tooltips
-				false // Configure chart to generate URLs?
-				);
-		try {
-				ChartUtilities.saveChartAsJPEG(new File("/home/colin_f/chart.jpg"), chart, 500, 300);
-		} catch (Exception e) {
-			System.out.println("Problem occurred creating chart.");
-			e.printStackTrace();
-		}
-		
-		DefaultCategoryDataset ds = new DefaultCategoryDataset();
-		DefaultCategoryDataset ds2 = new DefaultCategoryDataset();
-		//cal.add(Calendar.MONTH, -1);
+		pieValue += ((Integer)crit.list().size()).toString();
+		pieName += "5 et plus";
+		rqst.setAttribute("pieValue", pieValue);
+		rqst.setAttribute("pieName", pieName);
+		rqst.setAttribute("pieTitle", "Nombre moyen de livres par commande au cours des 6 derniers mois");
+
 		end = cal.getTime();
-		for (Integer i = 6; i >= 0; --i) {
+		String	barValue1	= new String();
+		String	barValue2	= new String();
+		String	barBottom	= "|6|5|4|3|2|1";
+		Integer	max1		= 0;
+		Integer	max2		= 0;
+		for (Integer i = 5; i >= 0; --i) {
 			begin = end;
 			cal.add(Calendar.MONTH, 1);
 			end = cal.getTime();
@@ -92,28 +92,38 @@ public class StatisticController
 				crit.add(Restrictions.between("date", (Date)(fo.parse(fo.format(begin))), (Date)(fo.parse(fo.format(end)))));
 			} catch (ParseException e) { e.printStackTrace(); }
 			List<?> l = crit.list();
-			ds.setValue(l.size(), "nbCommands", i.toString());
-			int sum = 0;
+			barValue1 += ((Integer)l.size()).toString();
+			if (l.size() > max1) { max1 = l.size(); }
+			//ds.setValue(l.size(), "nbCommands", i.toString());
+			Integer sum = 0;
 			for (Object elm : l) {
 				sum += ((Purchase) elm).getFullPrice();
 				//System.out.println("===>" + sum);
 			}
-			ds2.addValue(sum, "Total sold", i.toString());
+			System.out.println("===>" + l.size());
+			if (sum > max2) { max2 = sum; }
+			barValue2 += sum.toString();
+			//ds2.addValue(sum, "Total sold", i.toString());
+			if (i > 0) {
+				barValue1 += ",";
+				barValue2 += ",";
+			}
 		}
-		JFreeChart chart2 = ChartFactory.createBarChart("Numbers of command per month",
-				"Months ago", "Commands number", ds, PlotOrientation.VERTICAL,
-				false, true, false);
-		JFreeChart chart3 = ChartFactory.createBarChart("Total of prices per month",
-				"Months ago", "Total of commands prices ($)", ds2, PlotOrientation.VERTICAL,
-				false, true, false);
-		try {
-			ChartUtilities.saveChartAsJPEG(new File("/home/colin_f/chart2.jpg"), chart2, 500, 300);
-			ChartUtilities.saveChartAsJPEG(new File("/home/colin_f/chart3.jpg"), chart3, 500, 300);
-		} catch (Exception e) {
-			System.out.println("Problem occurred creating chart.");
-			e.printStackTrace();
+		String sMax1 = new String();
+		String sMax2 = new String();
+		for (int i = 0; i <= 4; ++i) {
+			sMax1 += ("|" + ((Integer)(max1 * i / 4)).toString());
+			sMax2 += ("|" + ((Double)(max2 / 4.0 * i)).toString());
 		}
-		System.out.println("===>" + System.getProperty("user.dir"));
+		System.out.println("::" + barValue1);
+		System.out.println("::" + max1);
+		rqst.setAttribute("bTitle1", "Nombre de commandes par mois (6 mois)");
+		rqst.setAttribute("bTitle2", "Total des commandes par mois (6 mois)");
+		rqst.setAttribute("barBottom", barBottom);
+		rqst.setAttribute("barValue1", barValue1);
+		rqst.setAttribute("barValue2", barValue2);
+		rqst.setAttribute("max1", sMax1);
+		rqst.setAttribute("max2", sMax2);
 		return "displayStat";
 	}
 
@@ -134,6 +144,8 @@ public class StatisticController
 		//System.out.println("begin:" + begin);
 		//System.out.println("end:" + end);
 		
+		String		pieValue	= new String();
+		String		pieName		= new String();
 		for (Integer i = 1; i < 5; ++i) {
 			crit = EMF.getSession().createCriteria(Purchase.class.getCanonicalName());
 			crit.add(Restrictions.eq("nbArticles", i));
@@ -142,7 +154,9 @@ public class StatisticController
 			} catch (ParseException e) { e.printStackTrace(); }
 			List<?>		l = crit.list();
 			//System.out.println("ICCICIIIIIIIIIIIIIIIIIIi =========>" + l.size());
-			pieDataSet.setValue(i.toString(), new Integer(l.size()));
+			//pieDataSet.setValue(i.toString(), new Integer(l.size()));
+			pieValue += ((Integer)l.size()).toString() + ",";
+			pieName += i.toString() + "|";
 			crit.list().clear();
 		}
 		crit = EMF.getSession().createCriteria(Purchase.class.getCanonicalName());
@@ -150,24 +164,18 @@ public class StatisticController
 		try {
 			crit.add(Restrictions.between("date", (Date)(fo.parse(fo.format(begin))), (Date)(fo.parse(fo.format(end)))));
 		} catch (ParseException e) { e.printStackTrace(); }
-		pieDataSet.setValue("5 et +", new Integer(crit.list().size()));
-		JFreeChart chart = ChartFactory.createPieChart
-				("Number book on a command during the last 6 months", // Title
-				pieDataSet, // Dataset
-				true, // Show legend
-				true, // Use tooltips
-				false // Configure chart to generate URLs?
-				);
-		try {
-				ChartUtilities.saveChartAsJPEG(new File("/home/colin_f/chart.jpg"), chart, 500, 300);
-		} catch (Exception e) {
-			System.out.println("Problem occurred creating chart.");
-			e.printStackTrace();
-		}
-		
-		DefaultCategoryDataset ds = new DefaultCategoryDataset();
-		DefaultCategoryDataset ds2 = new DefaultCategoryDataset();
+		pieValue += ((Integer)crit.list().size()).toString();
+		pieName += "5 et plus";
+		rqst.setAttribute("pieValue", pieValue);
+		rqst.setAttribute("pieName", pieName);
+		rqst.setAttribute("pieTitle", "Nombre moyen de livres par commande au cours des 7 derniers jours");
+
 		end = cal.getTime();
+		String	barValue1	= new String();
+		String	barValue2	= new String();
+		String	barBottom	= "|6|5|4|3|2|1";
+		Integer	max1		= 0;
+		Integer	max2		= 0;
 		for (Integer i = 6; i >= 0; --i) {
 			begin = end;
 			cal.add(Calendar.DAY_OF_WEEK, 1);
@@ -177,36 +185,39 @@ public class StatisticController
 				crit.add(Restrictions.between("date", (Date)(fo.parse(fo.format(begin))), (Date)(fo.parse(fo.format(end)))));
 			} catch (ParseException e) { e.printStackTrace(); }
 			List<?> l = crit.list();
-			ds.setValue(l.size(), "nbCommands", i.toString());
-			int sum = 0;
+			barValue1 += ((Integer)l.size()).toString();
+			if (l.size() > max1) { max1 = l.size(); }
+			//ds.setValue(l.size(), "nbCommands", i.toString());
+			Integer sum = 0;
 			for (Object elm : l) {
 				sum += ((Purchase) elm).getFullPrice();
 				//System.out.println("===>" + sum);
 			}
-			ds2.addValue(sum, "Total sold", i.toString());
+			System.out.println("===>" + l.size());
+			if (sum > max2) { max2 = sum; }
+			barValue2 += sum.toString();
+			//ds2.addValue(sum, "Total sold", i.toString());
+			if (i > 0) {
+				barValue1 += ",";
+				barValue2 += ",";
+			}
 		}
-		JFreeChart chart2 = ChartFactory.createBarChart("Numbers of command per week",
-				"Months ago", "Commands number", ds, PlotOrientation.VERTICAL,
-				false, true, false);
-		JFreeChart chart3 = ChartFactory.createBarChart("Total of prices per week",
-				"Months ago", "Total of commands prices ($)", ds2, PlotOrientation.VERTICAL,
-				false, true, false);
-		try {
-			ChartUtilities.saveChartAsJPEG(new File("/home/colin_f/chart2.jpg"), chart2, 500, 300);
-			ChartUtilities.saveChartAsJPEG(new File("/home/colin_f/chart3.jpg"), chart3, 500, 300);
-		} catch (Exception e) {
-			System.out.println("Problem occurred creating chart.");
-			e.printStackTrace();
+		String sMax1 = new String();
+		String sMax2 = new String();
+		for (int i = 0; i <= 4; ++i) {
+			sMax1 += ("|" + ((Integer)(max1 * i / 4)).toString());
+			sMax2 += ("|" + ((Double)(max2 / 4.0 * i)).toString());
 		}
-
+		System.out.println("::" + barValue1);
+		System.out.println("::" + max1);
+		rqst.setAttribute("bTitle1", "Nombre de commandes par jour (7 jours)");
+		rqst.setAttribute("bTitle2", "Total des commandes par jour (7 jours)");
+		rqst.setAttribute("barBottom", barBottom);
+		rqst.setAttribute("barValue1", barValue1);
+		rqst.setAttribute("barValue2", barValue2);
+		rqst.setAttribute("max1", sMax1);
+		rqst.setAttribute("max2", sMax2);
 		return "displayStat";
 	}
 
-//	@RequestMapping(
-//			value = "/daily",
-//			method = RequestMethod.GET)
-//	public String daily(HttpServletRequest rqst, HttpServletResponse resp, Model model)
-//	{
-//		return "displayStat";
-//	}
 }
